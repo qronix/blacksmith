b3b2c49b3c2c6476d45e6d78ecbf92cc098f9bdf
qronix <qroniix@gmail.com>
2019-12-07 20:39:31 -0500

    Safety commit before I destroy this CSS making the app responsive

--- src/providers/game.provider.jsx
+++ src/providers/game.provider.jsx
@@ -1,4 +1,4 @@
-import React, {createContext, useState, useEffect, useCallback, useRef} from 'react';
+import React, { createContext, useState, useEffect, useCallback, useRef } from 'react';
 
 import ITEMS from '../item-data';
 import UPGRADES from '../upgrades';
